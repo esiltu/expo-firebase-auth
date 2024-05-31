7a4c4b23c2c149d625050643d7d5aae454e9fbe4
ES <erays@192.168.68.100>
2024-05-31 18:27:43 +0200

    VacatureShift first commit

---
+++ app/(drawer)/index.tsx
@@ -0,0 +1,16 @@
+import { Stack } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+
+import { Container } from '~/components/Container';
+import { ScreenContent } from '~/components/ScreenContent';
+
+export default function Home() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Home' }} />
+      <Container>
+        <ScreenContent path="app/(drawer)/(tabs)/index.tsx" title="Home" />
+      </Container>
+    </>
+  );
+}
---
+++ metro.config.js
@@ -0,0 +1,7 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+// eslint-disable-next-line no-undef
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
