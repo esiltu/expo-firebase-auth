54abd902a6dcdea18bfcfdb262f4ac1baf742a98
ES <erays@192.168.68.100>
2024-05-31 19:44:52 +0200

    Working on Auth Flow

---
+++ app/auth/otp-login.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const OTPLogin = () => {
+    return (
+        <View>
+            <Text>otp-login</Text>
+        </View>
+    )
+}
+
+export default OTPLogin
+
+const styles = StyleSheet.create({})
