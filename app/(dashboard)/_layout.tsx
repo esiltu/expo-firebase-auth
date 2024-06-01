e4e071f05d924129b27cf54ce64dc7fe231e4432
ES <erays@192.168.68.100>
2024-06-01 17:03:24 +0200

    Tabs working fine

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -5,13 +5,29 @@ import { Ionicons } from "@expo/vector-icons";
 
 export default function DashboardLayout() {
     return (
-        <Tabs>
+        <Tabs screenOptions={{ headerShown: false }}>
             <Tabs.Screen
                 name="index"
                 options={{
-                    headerShown: false,
                     title: "Home",
+                    tabBarActiveTintColor: '#f3a683',
                     tabBarIcon: ({ color }) => <Ionicons size={28} name="home" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="diensten"
+                options={{
+                    title: "Diensten",
+                    tabBarActiveTintColor: '#f3a683',
+                    tabBarIcon: ({ color }) => <Ionicons size={28} name="clipboard-outline" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="settings"
+                options={{
+                    title: "Settings",
+                    tabBarActiveTintColor: '#f3a683',
+                    tabBarIcon: ({ color }) => <Ionicons size={28} name="settings-outline" color={color} />,
                 }}
             />
         </Tabs>
---
+++ app/(dashboard)/settings.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const settings = () => {
+    return (
+        <View>
+            <Text>settings</Text>
+        </View>
+    )
+}
+
+export default settings
+
+const styles = StyleSheet.create({})
