7c789baaf5022b948e365eb003a871f3acfeaf3c
Eray Siltu <eray.siltu@athom.nl>
2024-06-30 22:22:21 +0200

    feat: (i18n localization & expo) setup done

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,25 +1,88 @@
-import React from "react";
-import { View, Text, Button, SafeAreaView, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, SafeAreaView, StyleSheet, Image, Alert, Platform, TouchableOpacity, Modal } from "react-native";
 import { useRouter } from "expo-router";
+import i18n from "~/hooks/useTranslation";
+import * as Updates from 'expo-updates';
 
-const AuthIndex = () => {
+export default function AuthIndex() {
+
     const router = useRouter();
+
+    // useTranslation hook descruction
+    const { t } = i18n;
+
+    // Custom hooks for i18n
+    const [selectedLanguage, setSelectedLanguage] = useState('en');
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const changeLanguage = (lng: string) => {
+        setSelectedLanguage(lng);
+        i18n.changeLanguage(lng);
+        setModalVisible(false);
+        const language = lng === 'en' ? 'English' : 'Dutch';
+        Alert.alert(
+            "Language Changed",
+            `The language has been changed to ${language}. Please restart the app for the changes to take effect.`,
+            [
+                {
+                    text: "Restart Now",
+                    onPress: () => {
+                        Updates.reloadAsync();
+                    },
+                },
+                { text: "Later" },
+            ]
+        );
+    };
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.headerContainer}>
-                <Text style={styles.title}>Expo Firebase Auth</Text>
-                <Text style={styles.subtitle}>with TypeScript and Expo Router</Text>
+            <View style={styles.header}>
+                <TouchableOpacity style={styles.languageContainer} onPress={() => setModalVisible(true)}>
+                    <Image
+                        source={
+                            selectedLanguage === 'en'
+                                ? require('assets/flags/locale-english-flag.png')
+                                : require('assets/flags/locale-dutch-flag.png')
+                        }
+                        style={styles.flag}
+                    />
+                    <Text style={styles.languageText}>
+                        {selectedLanguage === 'en' ? 'English' : 'Dutch'} 🌐
+                    </Text>
+                </TouchableOpacity>
             </View>
-            <View style={styles.section}>
-                <Text style={styles.header}>Login</Text>
-                <Text style={styles.description}>Welcome! Log in to access our services.</Text>
-                <Button title="Sign In" color="#FF7043" onPress={() => router.navigate('/sign-in')} />
-            </View>
-            <View style={styles.section}>
-                <Text style={styles.header}>Register</Text>
-                <Text style={styles.description}>Don't have an account? Sign up now!</Text>
-                <Button title="Sign Up" color="#FF7043" onPress={() => router.navigate('/sign-up')} />
+            <Modal
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View style={styles.modalOverlay}>
+                    <View style={styles.modalContainer}>
+                        <TouchableOpacity style={styles.modalItem} onPress={() => changeLanguage('en')}>
+                            <Image source={require('assets/flags/locale-english-flag.png')} style={styles.modalFlag} />
+                            <Text style={styles.modalText}>English</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.modalItem} onPress={() => changeLanguage('nl')}>
+                            <Image source={require('assets/flags/locale-dutch-flag.png')} style={styles.modalFlag} />
+                            <Text style={styles.modalText}>Dutch</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+            <View style={styles.content}>
+                <Text style={styles.title}>{t('home-title')}</Text>
+                <Text style={styles.subtitle}>{t('home-subtitle')}</Text>
+                <View style={styles.card}>
+                    <Text style={styles.cardHeader}>{t('home-login-text')}</Text>
+                    <Text style={styles.cardDescription}>{t('home-login-description-text')}</Text>
+                    <Button title={t('home-login-text')} color="#FF7043" onPress={() => router.navigate('/sign-in')} />
+                </View>
+                <View style={styles.card}>
+                    <Text style={styles.cardHeader}>{t('home-register-text')}</Text>
+                    <Text style={styles.cardDescription}>{t('home-register-description-text')}</Text>
+                    <Button title={t('home-register-text')} color="#FF7043" onPress={() => router.navigate('/sign-up')} />
+                </View>
             </View>
         </SafeAreaView>
     );
@@ -28,51 +91,109 @@ const AuthIndex = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#ECEFF1',
-        padding: 20,
-    },
-    headerContainer: {
-        alignItems: 'center',
-        marginBottom: 30,
-    },
-    title: {
-        fontSize: 28,
-        fontWeight: 'bold',
-        color: '#00796B',
-        textAlign: 'center',
-    },
-    subtitle: {
-        fontSize: 18,
-        color: '#00796B',
-        textAlign: 'center',
-    },
-    section: {
-        width: '90%',
-        padding: 20,
-        marginBottom: 20,
         backgroundColor: '#FFFFFF',
-        borderRadius: 10,
+    },
+    header: {
+        paddingTop: Platform.OS === 'ios' ? 60 : 40,
+        paddingBottom: 20,
+        paddingHorizontal: 20,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    },
+    languageContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#F0F0F0',
+        borderRadius: 20,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.2,
         shadowRadius: 2,
         elevation: 2,
     },
-    header: {
+    flag: {
+        width: 24,
+        height: 24,
+        marginRight: 10,
+    },
+    languageText: {
+        fontSize: 16,
+        color: '#000',
+    },
+    modalOverlay: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    modalContainer: {
+        width: 250,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        padding: 20,
+        alignItems: 'center',
+    },
+    modalItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 10,
+        width: '100%',
+    },
+    modalFlag: {
+        width: 30,
+        height: 30,
+        marginRight: 10,
+    },
+    modalText: {
+        fontSize: 18,
+        color: '#000',
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#FF7043',
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    subtitle: {
+        fontSize: 18,
+        color: '#FF7043',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    card: {
+        width: '100%',
+        maxWidth: 340,
+        padding: 20,
+        marginVertical: 10,
+        backgroundColor: '#FFF3E0',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+        elevation: 2,
+        alignItems: 'center',
+    },
+    cardHeader: {
         fontSize: 24,
         fontWeight: 'bold',
+        color: '#FF7043',
         marginBottom: 10,
-        color: '#00796B',
-        textAlign: 'left',
+        textAlign: 'center',
     },
-    description: {
+    cardDescription: {
         fontSize: 16,
+        color: '#757575',
         marginBottom: 20,
-        color: '#607D8B',
-        textAlign: 'left',
+        textAlign: 'center',
     },
 });
-
-export default AuthIndex;
